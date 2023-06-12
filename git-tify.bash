319a885816a4883d8c8ebf73ac67dc68ad600b60
Ralf Stephan <me@ralf-stephan.name>
2023-06-12 06:31:21 +0200

    Date.
    * config.inc.bash.example, git-tify.bash: change keyword Date to keyword Version.

--- git-tify.bash
+++ git-tify.bash
@@ -663,11 +663,12 @@ function main()
 
   # The Version of this script.
 
-  local version="$( cat << 'END' 
+  local version="$( cat << 'END'
 git-tify.bash (RaSt git-gnu-project)
 0.2.0 (2023-06-12 03:10:51 +00:00:00)
 Copyright (C)  2023  Ralf Stephan  <me@ralf-stephan.name>
-License GPLv3+ (GNU GPL version 3 or later, see <https://gnu.org/licenses/gpl.html>)
+License GPLv3+ (GNU GPL version 3 or later,
+see <https://gnu.org/licenses/gpl.html>)
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
 END

ee55490af90c9a569f2b5813740d3d4448167b5c
Ralf Stephan <me@ralf-stephan.name>
2023-06-13 08:08:28 +0200

    Copyright and license notice.
    * gnu-tify.bash: add copyright and license notice.

--- gnu-tify.bash
+++ gnu-tify.bash
@@ -1,1 +1,24 @@
 #! /usr/bin/env -S bash -e
+################################################################################
+##
+## This file is part of the RaSt git-gnu-project package.
+##
+## RaSt  git-gnu-project is  to manage  a directory  as a  Git and  a GNU  style
+## project.
+##
+## Copyright (C)  2023  Ralf Stephan  <me@ralf-stephan.name>
+##
+## RaSt git-gnu-project is free software:  you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by the Free
+## Software Foundation, either version 3 of the License, or (at your option) any
+## later version.
+##
+## RaSt git-gnu-project is  distributed in the hope that it  will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR  A PARTICULAR PURPOSE.   See the  GNU General Public  License for
+## more details.
+##
+## You should have received a copy of  the GNU General Public License along with
+## this package.  If not, see <https://www.gnu.org/licenses/>.
+##
+################################################################################

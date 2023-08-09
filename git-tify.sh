556a1ca22c1ac25d6ee434a4145bb5519b2f49f2
Ralf Stephan <me@ralf-stephan.name>
2023-08-09 10:00:33 +0200

    Fix whitespace.
    * .gitignore, .gitkeep, README, config.inc.sh.example, git-tify.sh,
    gnu-tify.sh, hooks/.gitkeep, licenses/.gitkeep, templates/.gitkeep: fix
    whitespace.

--- git-tify.sh
+++ git-tify.sh
@@ -21,7 +21,7 @@
 ## You should have received  a copy of the  GNU General Public License  along ##
 ## with this package.  If not, see <https://www.gnu.org/licenses/>.           ##
 ##                                                                            ##
-## $Version: 0.10.0 (2023-08-08 07:34:01 +00:00:00)$                          ##
+## $Version: 0.10.0 (2023-08-08 07:34:01 +00:00:00) $                         ##
 ##                                                                            ##
 ################################################################################
 
